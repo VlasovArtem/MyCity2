ca97b03732adf2dd389de4b9fe1a19d72bd5b43b
VlasovArtem <vlasovartem21@gmail.com>
2014-11-13 02:52:06 +0200

    Refactoring several classes

--- MyCity2/mycity-persistence/src/main/java/org/hillel/it/mycity/persistence/AppConfig.java
+++ MyCity2/mycity-persistence/src/main/java/org/hillel/it/mycity/persistence/AppConfig.java
@@ -56,11 +56,8 @@ public class AppConfig {
 		bean.setDataSource(dataSource);
 		Properties properties = new Properties();
 		properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		properties.put("hibernate.cache.use_second_level_cache", env.getProperty("second.level.cache"));
-		properties.put("hibernate.cache.provider_class", env.getProperty("cache.provider.class"));
 		properties.put("hibernate.hbm2ddl.auto", env.getProperty("hbm2ddl.auto"));
 		properties.put("hibernate.show_sql", env.getProperty("show_sql"));
-		properties.put("hibernate.chache.region.factory_class", env.getProperty("cache.region.class"));
 		bean.setHibernateProperties(properties);
 		bean.setEntityInterceptor(new GlobalInterceptor());
 		return bean;
--- MyCity2/mycity-services/src/test/java/org/hillel/it/mycity/resource/TestResourceTest.java
+++ MyCity2/mycity-services/src/test/java/org/hillel/it/mycity/resource/TestResourceTest.java
@@ -5,33 +5,35 @@ import java.util.Set;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Application;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.SpringLifecycleListener;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.jetty.JettyTestContainerFactory;
 import org.glassfish.jersey.test.spi.TestContainerException;
 import org.glassfish.jersey.test.spi.TestContainerFactory;
 import org.hillel.it.mycity.persistence.AppConfig;
+import org.hillel.it.mycity.rest.AppInitializer;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.google.common.collect.ImmutableSet;
-
 @Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AppConfig.class} )
+@ContextConfiguration(classes = {AppConfig.class, AppInitializer.class} )
 public class TestResourceTest extends JerseyTest{
-
+	
 	@Override
 	protected Application configure() {
-		return new Application(){
-			@Override
-			public Set<Class<?>> getClasses() {
-				return ImmutableSet.<Class<?>>of(TestResource.class);
-			}
-			
-		};
+		ResourceConfig rc = new ResourceConfig();
+		rc.registerClasses(TestResource.class)
+		.property("contextConfig", new AnnotationConfigApplicationContext(AppConfig.class))
+		.register(SpringLifecycleListener.class)
+		.register(RequestContextFilter.class);
+		return rc;
 	}
 
 	@Override
@@ -42,12 +44,9 @@ public class TestResourceTest extends JerseyTest{
 
 	@Test
 	public void getId() {
-		WebTarget target = client().target("http://localhost:9998/");
+		WebTarget target = client().target("http://localhost:8080/");
 		String string = target.path("hello").request().get(String.class);
 		System.out.println(string);
 	}
-	
-	
-	
-	
+
 }
